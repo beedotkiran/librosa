7c35f86bc275c9b2276febfcdb91369d2872c43e
Ravi Kiran <beedotkiran@gmail.com>
2016-06-30 16:30:55 +0200

    corrected variable names

--- librosa/core/scattering.py
+++ librosa/core/scattering.py
@@ -1,26 +1,24 @@
-def getMotherFrequency(NFO):
+def getMotherFrequency(bins_per_octave):
 	"""
-	The dimensionless mother center frequency `ξ` (corresponding to a log-period
-	`γ=0`) is computed as the midpoint between the center frequency of the second
-	center frequency `ξ*2^(-1/nFilters_per_octave)` (corresponding to `γ=1`) and the
-	negative mother center frequency `(1-ξ)`. Hence the equation
-	`2ξ = ξ*2^(-1/nFilters_per_octave) + (1-ξ)`, of which we
-	derive `ξ = 1 / (3 - 2^(1/nFilters_per_octave))`. This formula is valid
+	The dimensionless mother center frequency '\xi' (corresponding to a log-period
+	'\gamma=0') is computed as the midpoint between the center frequency of the second
+	center frequency '\xi*2^(-1/bins_per_octave)' (corresponding to '\gamma=1') and the
+	negative mother center frequency '(1-\xi)'. Hence the equation
+	'2\xi = \xi*2^(-1/bins_per_octave) + (1-\xi)', of which we
+	derive '\xi = 1 / (3 - 2^(1/bins_per_octave))'. This formula is valid
 	only when the wavelet is a symmetric bump in the Fourier domain.
 	
 	Function returns the dimensionless mother frequency 
 	
 	Inputs 
 	======
-	NFO : Number of Filters per Octave (this is Q in the paper)
+	bins_per_octave : Number of Filters per Octave 
+	
 	Outputs
 	=======
-	motherFrequency
+	motherFrequency : dimensionaless mother center frequency
 	
 	"""
 
-	motherFrequency = 1/(3-2**(-1/NFO))
+	motherFrequency = 1/(3-2**(-1/bins_per_octave))
 	return motherFrequency
-
-
-
